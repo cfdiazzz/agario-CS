5df7c5688a1dc3064fba7ee857bd131bcf62f21d
Christian F Diaz <cfdiaz@csuchico.edu>
2026-07-14 10:38:12 -0700

    Test run worked

--- world.cpp
+++ world.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-#include "raylib.h"
-int main() {
-    Color White = {255, 255 , 255 , 255};
-    InitWindow(900, 600, "Flappymon");
-    SetTargetFPS(60);
-    while(WindowShouldClose() == false) {
-        BeginDrawing();
-        ClearBackground(White);
-        EndDrawing();
-    }
+// #include <iostream>
+// #include "raylib.h"
+// int main() {
+//     Color White = {255, 255 , 255 , 255};
+//     InitWindow(900, 600, "Flappymon");
+//     SetTargetFPS(60);
+//     while(WindowShouldClose() == false) {
+//         BeginDrawing();
+//         ClearBackground(White);
+//         EndDrawing();
+//     }
 
-CloseWindow();
+// CloseWindow();
 
-}
+// }
