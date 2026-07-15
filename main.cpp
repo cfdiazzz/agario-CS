1aecbf73902170c9567f123019884eb9396a06bb
Angel G Perez Sanchez <agperezsanchez@csuchico.edu>
2026-07-14 21:08:20 -0700

    Copied my circle absorption code onto VS code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include "raylib.h"
 
+int main() {
+    // 1. Open a blank window (Width, Height, Title)
+    InitWindow(800, 450, "Raylib Sync Test");
+    SetTargetFPS(60);
+
+    // 2. Main game loop
+    while (!WindowShouldClose()) {
+        // 3. Drawing phase
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        
+        // Draw text on the screen: (Text, X, Y, FontSize, Color)
+        DrawText("If you see this, Raylib is 100% working!", 140, 200, 24, LIGHTGRAY);
+    
+        EndDrawing();
+    }
+
+    // 4. Close the window and clear memory
+    CloseWindow();
+    return 0;
+}
